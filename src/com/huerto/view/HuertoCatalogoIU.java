b1fe6c876747153f2c9a978c2bf0c261fc505de0
Lucia Balsa <lbalsamartinez@danielcastelao.org>
2023-06-05 17:52:41 +0200

    Pruebas extra

--- src/com/huerto/view/HuertoCatalogoIU.java
+++ src/com/huerto/view/HuertoCatalogoIU.java
@@ -12,6 +12,7 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
 import java.util.ArrayList;
 
 /**
